04f049a39331334cfef869c73c8d5a213b35bd1f
samuel <samuellino2006@gmail.com>
2025-10-25 17:02:19 -0300

    configuração inicial

---
+++ src/styles/index.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const estiloGlobal = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+  }
+`
